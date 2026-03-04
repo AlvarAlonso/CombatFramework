b4017ed644d910125e56f095351b7efe28c6440b
= <alvar.alonso.2010@gmail.com>
2026-03-04 08:38:49 +0100

    create suicidal peasant abilities.

---
+++ project/Source/CombatFramework/Public/AbilitySystem/GameplayAbilities/CFR_GA_SuicidalExplosion.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/GameplayAbilities/CFR_GA_PlayMontage.h"
+#include "CFR_GA_SuicidalExplosion.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMBATFRAMEWORK_API UCFR_GA_SuicidalExplosion : public UCFR_GA_PlayMontage
+{
+	GENERATED_BODY()
+	
+};
